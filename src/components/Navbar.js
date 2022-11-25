0613c2f499e85f797c81f4571a1b9c0b07009a79
mobilizer4you <d.m.risingdeveloper@gmail.com>
2022-11-24 20:19:54 -0500

    edited Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://blogs.soumya-jit.tech/"
+                href="https://"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/alexbaker217/alexbaker217"
                 target="_blank"
                 className="fork-btn-inner"
               >
