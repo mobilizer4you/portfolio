936cd191c2674b25469549b9f7f5bbe28745c9c9
mobilizer4you <d.m.risingdeveloper@gmail.com>
2022-11-24 23:01:57 -0500

    changed resume page

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,11 +8,11 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-// const resumeLink =
-//   "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-
 const resumeLink =
-  "";
+  "https://raw.githubusercontent.com/mobilizer4you/portfolio/master/src/Assets/baker.jpeg";
+
+// const resumeLink =
+//   "";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
